61b070d9bd8e39a8270bda89cd7de3ea318e981f
Carlos Rafael Gimenes das Neves <carlosrafael.prog@gmail.com>
2019-10-10 19:06:27 -0300

    Minor UI adjustments

--- routes/participante.ts
+++ routes/participante.ts
@@ -113,7 +113,7 @@ router.all("/certificado/:i", wrap(async (req: express.Request, res: express.Res
 			} else {
 				let presencas = await Participante.listarPresencas(ids[0], ids[1]);
 				if (!presencas || !presencas.length)
-					res.render("shared/erro-fundo", { layout: "layout-externo", imagemFundo: true, titulo: "Erro de Certificado", mensagem: "O participante não possui presenças no evento " + evento.nome });
+					res.render("shared/erro-fundo", { layout: "layout-externo", imagemFundo: true, titulo: "Erro de Certificado", mensagem: "Ainda sem presenças marcadas no evento " + evento.nome });
 				else
 					res.render("participante/certificado", { layout: "layout-externo", imagemFundo: true, titulo: "Certificado de Participação", palestrante: false, participante: participante, evento: evento, idcertificado: idcertificado, presencas: JSON.stringify(presencas) });
 			}
