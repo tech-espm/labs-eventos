31253904393c227dca0c06cb29eff7d753ad0485
Carlos Rafael Gimenes das Neves <carlosrafael.prog@gmail.com>
2020-01-15 08:10:22 -0300

    Adding new features to the side bar

--- public/js/admin-1.0.6.js
+++ public/js/admin-1.0.6.js
@@ -1,14 +1,50 @@
 "use strict";
 
 (function () {
-	var sideMenu = $("#side-menu");
-	if (sideMenu && sideMenu.length)
-		sideMenu.metisMenu();
+	var sideMenu = _("side-menu"),
+		sidebarClassName = "sidebar",
+		sidebar = _("sidebar"),
+		btnSidebar = _("btn-sidebar"),
+		btnSidebarShow = null,
+		sidebarFakeBg = _("sidebar-fake-bg"),
+		pageWrapper = _("page-wrapper");
+
+	if (sideMenu)
+		$(sideMenu).metisMenu();
+
+	if (btnSidebar) {
+		btnSidebar.onclick = function () {
+			if (btnSidebarShow)
+				return;
+			sidebar.className = sidebarClassName + " visible-xs-block";
+			sidebarFakeBg.className = "sidebar-fake-bg hidden";
+			sideMenu.className = "nav visible-xs-block";
+			pageWrapper.className = "fullscreen nosidebar";
+			btnSidebarShow = document.createElement("button");
+			btnSidebarShow.className = "btn btn-outline btn-default btn-sidebar btn-sidebar-show hidden-xs";
+			btnSidebarShow.setAttribute("title", "Exibir Menu");
+			var i = document.createElement("i");
+			i.className = "fa fa-bars fa-fw fa-nomargin";
+			btnSidebarShow.appendChild(i);
+			btnSidebarShow.onclick = function () {
+				if (!btnSidebarShow)
+					return;
+				sidebar.parentNode.removeChild(btnSidebarShow);
+				btnSidebarShow = null;
+				sidebar.className = sidebarClassName;
+				sidebarFakeBg.className = "sidebar-fake-bg";
+				sideMenu.className = "nav";
+				pageWrapper.className = "";
+			};
+			sidebar.parentNode.appendChild(btnSidebarShow);
+		};
+	}
 
 	if (navigator.userAgent.indexOf("Chrome") <= 0 && navigator.userAgent.indexOf("Safari") > -1) {
 		$(function () {
-			var sidebarFakeBg = _("sidebar-fake-bg"), l = 0;
-			_("sidebar").className = "sidebar-safari sidebar";
+			var l = 0;
+			sidebarClassName = "sidebar-safari sidebar";
+			sidebar.className = sidebarClassName;
 			$(window).bind("load scroll", function () {
 				var n = (window.pageYOffset <= 10 ? 125 : 0);
 				if (l !== n) {
